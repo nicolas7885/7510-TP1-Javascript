93d970dfe9d148c9ac50007294b823ad4f0d3803
nicolas7885 <nicolas7885@hotmail.com>
2017-10-08 23:07:56 -0300

    Database validation
    You will start noticing copy pasting of code here(commits times are so close). That is because i did this before frok was available and i am only now doing things with tests you provided, and commits.

--- src/interpreter.js
+++ src/interpreter.js
@@ -13,6 +13,82 @@ var Interpreter = function () {
 
 	const FACT_REGEX= /([a-zA-Z]*)(\(([\w\s,$]*)\))/g;
 
+	function databaseToLines(database){
+	  return database; //change this if its a file
+	}
+
+	function parseGroup(group){
+	  var returnValue = FACT_REGEX.exec(group);
+	  FACT_REGEX.lastIndex=0;
+	  return returnValue;
+	}
+
+	//returns the list of parameters
+	function getParameters(group){
+	  group=group.replace(/[\.\s]/i, '');
+	  return parseGroup(group)[3].split(",");
+	}
+
+	//returns true if every element of input exists in coll
+	function allPresentIn(input, coll){
+	  return input.every(function(value){
+	    return coll.includes(value);
+	  });
+	}
+
+	//return true if rule is valid, false if not
+	function validateRule(ruleLine){
+	  var parsedRule= ruleLine.match(FACT_REGEX);
+	  if(!ruleLine.match(/:-/)){
+	    return false;
+	  }
+	  var intendedFactDelimeterRegex= /(\)[\s]*,)|(,[\s]*\w*\()/gi;
+	  numberOfIntendedFacts= (ruleLine.match(intendedFactDelimeterRegex) || []).length +1;
+	  if (parsedRule.length < (numberOfIntendedFacts + 1)){
+	    return false;
+	  }
+	  var rule= parsedRule.shift();
+	  var facts= parsedRule;
+	  var valid= validateFact(rule);
+	  var i=0;
+	  while (valid && i<facts.length){
+	    valid = valid &&
+	      validateFact(facts[i]) &&
+	      allPresentIn(getParameters(facts[i]), getParameters(rule));
+	    i++
+	  }
+	  return valid;
+	}
+
+	//return true if fact is valid, false if not
+	function validateFact(fact){
+	  var notEmpty=function(a){return a;};
+	  if (fact.match(FACT_REGEX) && getParameters(fact).every(notEmpty))
+	    return true;
+	  else
+	    return false;
+	}
+
+	//returns true if line is valid, false if not
+	function validateLine(line){
+	  if (!line)
+	    return true;
+	  if (line.match(/[:-]/i))
+	    return validateRule(line);
+	  else
+	    return validateFact(line);
+	}
+
+	//return true if database is valid, false if not
+	function validateDatabase(database){
+	  var lines= databaseToLines(database);
+	  return lines.every(validateLine);
+	}
+
+	//return true if query is valid, false if not
+	function validateQuery(query){
+	  return query && validateFact(query);
+	}
 }
 
 module.exports = Interpreter;
